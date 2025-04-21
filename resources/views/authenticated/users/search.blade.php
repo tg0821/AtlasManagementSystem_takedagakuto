03ab790e6e552badec0094f40a5883bd351ac958
武田楽斗 <>
2025-04-21 22:55:48 +0900

    選択科目の表示

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -42,9 +42,16 @@
         @endif
       </div>
       <div>
-        @if($user->role == 4)
-        <span>選択科目 :</span>
+       @if($user->role == 4)
+         <span>選択科目 :</span>
+        @if($user->subjects->isNotEmpty())
+         @foreach($user->subjects as $subject)
+         <span>{{ $subject->subject }}</span>
+         @endforeach
+        @else
+         <span>（なし）</span>
         @endif
+       @endif
       </div>
     </div>
     @endforeach
