5c46f465ff051899d40c47fc04a9e32e6baefd52
武田楽斗 <>
2025-04-29 18:09:46 +0900

    複数選択科目を選んだ際にユーザー検索で表示されるようにしました

--- app/Searchs/SelectNameDetails.php
+++ app/Searchs/SelectNameDetails.php
@@ -5,34 +5,66 @@ use App\Models\Users\User;
 
 class SelectNameDetails implements DisplayUsers{
 
-  // 改修課題：選択科目の検索機能
+  // // 改修課題：選択科目の検索機能
+  // public function resultUsers($keyword, $category, $updown, $gender, $role, $subjects){
+  //   if(is_null($gender)){
+  //     $gender = ['1', '2', '3'];
+  //   }else{
+  //     $gender = array($gender);
+  //   }
+  //   if(is_null($role)){
+  //     $role = ['1', '2', '3', '4'];
+  //   }else{
+  //     $role = array($role);
+  //   }
+  //   $users = User::with('subjects')
+  //   ->where(function($q) use ($keyword){
+  //     $q->Where('over_name', 'like', '%'.$keyword.'%')
+  //     ->orWhere('under_name', 'like', '%'.$keyword.'%')
+  //     ->orWhere('over_name_kana', 'like', '%'.$keyword.'%')
+  //     ->orWhere('under_name_kana', 'like', '%'.$keyword.'%');
+  //   })
+  //   ->where(function($q) use ($role, $gender){
+  //     $q->whereIn('sex', $gender)
+  //     ->whereIn('role', $role);
+  //   })
+  //   ->whereHas('subjects', function($q) use ($subjects){
+  //     $q->where('subjects.id', $subjects);
+  //   })
+  //   ->orderBy('over_name_kana', $updown)->get();
+  //   return $users;
+  // }
+
   public function resultUsers($keyword, $category, $updown, $gender, $role, $subjects){
     if(is_null($gender)){
-      $gender = ['1', '2', '3'];
+        $gender = ['1', '2', '3'];
     }else{
-      $gender = array($gender);
+        $gender = array($gender);
     }
     if(is_null($role)){
-      $role = ['1', '2', '3', '4'];
+        $role = ['1', '2', '3', '4'];
     }else{
-      $role = array($role);
+        $role = array($role);
     }
+
     $users = User::with('subjects')
-    ->where(function($q) use ($keyword){
-      $q->Where('over_name', 'like', '%'.$keyword.'%')
-      ->orWhere('under_name', 'like', '%'.$keyword.'%')
-      ->orWhere('over_name_kana', 'like', '%'.$keyword.'%')
-      ->orWhere('under_name_kana', 'like', '%'.$keyword.'%');
-    })
-    ->where(function($q) use ($role, $gender){
-      $q->whereIn('sex', $gender)
-      ->whereIn('role', $role);
-    })
-    ->whereHas('subjects', function($q) use ($subjects){
-      $q->where('subjects.id', $subjects);
-    })
-    ->orderBy('over_name_kana', $updown)->get();
+        ->where(function($q) use ($keyword){
+            $q->where('over_name', 'like', '%'.$keyword.'%')
+              ->orWhere('under_name', 'like', '%'.$keyword.'%')
+              ->orWhere('over_name_kana', 'like', '%'.$keyword.'%')
+              ->orWhere('under_name_kana', 'like', '%'.$keyword.'%');
+        })
+        ->where(function($q) use ($role, $gender){
+            $q->whereIn('sex', $gender)
+              ->whereIn('role', $role);
+        })
+        ->whereHas('subjects', function($q) use ($subjects){
+            $q->whereIn('subjects.id', $subjects);  // here is the change: OR search on subjects
+        })
+        ->orderBy('over_name_kana', $updown)
+        ->get();
+
     return $users;
-  }
+}
 
 }
